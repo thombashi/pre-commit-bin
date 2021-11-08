42e698548bcb48d5e7e9f57a367322b00f0ea2ff
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-11-08 14:42:01 +0900

    Rename a variable

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -3,7 +3,7 @@
 LATST_RELEASE_URL=https://api.github.com/repos/thombashi/pre-commit-bin/releases/latest
 INSTALL_DIR_PATH=/usr/local/bin/
 VERSION_CODENAME=$(\grep -Po "(?<=VERSION_CODENAME=)[a-z]+" /etc/os-release)
-URL=$(curl -sSL "$LATST_RELEASE_URL" | jq -r ".assets[] | select(.name | contains(\"_linux_${VERSION_CODENAME}\")) | .browser_download_url")
-curl -SL "$URL" | tar xzf - -C "$INSTALL_DIR_PATH"
+TARBALL_URL=$(curl -sSL "$LATST_RELEASE_URL" | jq -r ".assets[] | select(.name | contains(\"_linux_${VERSION_CODENAME}\")) | .browser_download_url")
+curl -SL "$TARBALL_URL" | tar xzf - -C "$INSTALL_DIR_PATH"
 
 "${INSTALL_DIR_PATH}"/pre-commit --version
