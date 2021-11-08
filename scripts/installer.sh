1f612c1298ee19138aea6f4f54aed6eccb6b1e33
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-11-08 14:39:26 +0900

    Extract a variable definition

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+LATST_RELEASE_URL=https://api.github.com/repos/thombashi/pre-commit-bin/releases/latest
 INSTALL_DIR_PATH=/usr/local/bin/
 VERSION_CODENAME=$(\grep -Po "(?<=VERSION_CODENAME=)[a-z]+" /etc/os-release)
-URL=$(curl -sSL https://api.github.com/repos/thombashi/pre-commit-bin/releases/latest | jq -r ".assets[] | select(.name | contains(\"_linux_${VERSION_CODENAME}\")) | .browser_download_url")
+URL=$(curl -sSL "$LATST_RELEASE_URL" | jq -r ".assets[] | select(.name | contains(\"_linux_${VERSION_CODENAME}\")) | .browser_download_url")
 curl -SL "$URL" | tar xzf - -C "$INSTALL_DIR_PATH"
 
 "${INSTALL_DIR_PATH}"/pre-commit --version
